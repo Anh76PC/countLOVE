5405e7575b49f4e260a806f2f075582e1104caa2
anh76pc <anhchomoi249@gmail.com>
2023-02-20 01:05:44 +0700

    push1

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ music = ['myheart','betifulinwhite','mylove'];
 document.addEventListener('DOMContentLoaded', function(){
       var rootTime = document.querySelector("time");
 
-      document.querySelector("anni").textContent = `${(yourDate.getDate()>9)?yourDate.getDate():"0"+yourDate.getDate()}-${(yourDate.getMonth()>8)?(yourDate.getMonth()+1):"0"+(yourDate.getMonth()+1)}-${yourDate.getFullYear()}`;
+      document.querySelector("anni").textContent = "11-11-2022";
       
       document.querySelector("date").textContent = Math.floor( Math.floor((new Date() - yourDate) / 1000) / 60 / 60 / 24)+" DAYS";
 
